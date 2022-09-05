3c015823ef8138a2c10bd871e27daa91269dfebe
jass921026 <62426283+jass921026@users.noreply.github.com>
2022-09-05 20:08:37 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n>1){
+        cout<<n<<endl;
+        if(n%2==0){
+            n = n/2;
+        } else{
+            n = 3*n+1;
+        }
+    }
+    cout<<n<<endl;
+    return 0;
+}
