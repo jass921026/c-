1caa54b2c94d89e478cedd9e8819b789d47aa57f
jass921026 <62426283+jass921026@users.noreply.github.com>
2022-11-06 16:35:16 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    int n, count = 0;
     cin>>n;
     while(n>1){
         cout<<n<<endl;
+        count += 1;
         if(n%2==0){
             n = n/2;
         } else{
@@ -12,5 +13,6 @@ int main(){
         }
     }
     cout<<n<<endl;
+    cout<<"count:"<<count+1<<endl;
     return 0;
 }
